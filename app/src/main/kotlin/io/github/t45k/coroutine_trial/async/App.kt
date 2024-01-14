127237d234a4581e404f3907c61b8d5242237bd8
T45K <tasktas9@gmail.com>
2024-01-14 18:27:26 +0900

    update

--- app/src/main/kotlin/io/github/t45k/coroutine_trial/async/App.kt
+++ app/src/main/kotlin/io/github/t45k/coroutine_trial/async/App.kt
@@ -1,16 +1,19 @@
 package io.github.t45k.coroutine_trial.async
 
 import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.delay
 import kotlin.system.measureTimeMillis
 
 suspend fun main() = coroutineScope {
-    val deferred1 = async { delay(1000) }
-    val deferred2 = async { delay(1000) }
-    val time = measureTimeMillis {
-        deferred1.await()
-        deferred2.await()
-    }
-    println(time)
+    println(measureTimeMillis {
+        (0..<1_000_000).map {
+            async {
+                println("start - $it - ${Thread.currentThread().name}")
+                delay(1_000)
+                println("end - $it - ${Thread.currentThread().name}")
+            }
+        }.awaitAll()
+    })
 }
